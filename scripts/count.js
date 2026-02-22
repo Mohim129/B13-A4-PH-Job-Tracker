51fc575e202a9ed1d47e2262aba04c40bea33bc3
Mohim129 <countrysidefarm15@gmail.com>
2026-02-22 14:59:56 +0600

    job count added

---
+++ scripts/count.js
@@ -0,0 +1,17 @@
+let totalCount = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectCount = document.getElementById("rejected-count");
+let totalCount2 = document.getElementById('total-count-2')
+
+const allJobs = document.getElementById('all-jobs-cards');
+const allJobs2 = document.getElementById('all-jobs-cards');
+const interviewJobs = document.getElementById("interview-jobs-cards");  
+const rejectedJobs = document.getElementById('rejected-jobs-cards')  
+
+function calculateCount(){
+    totalCount.innerText = Number(allJobs.children.length);
+    totalCount2.innerText = Number(allJobs.children.length);
+    interviewCount.innerText = Number(interviewJobs.children.length);
+    rejectCount.innerText = Number(rejectedJobs.children.length);
+}
+calculateCount()
