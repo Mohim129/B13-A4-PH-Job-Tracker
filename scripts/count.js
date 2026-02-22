96d97671410084afe427cb0a25d7192d9a97b65d
Mohim129 <countrysidefarm15@gmail.com>
2026-02-22 20:26:40 +0600

    sections are now changing on click

--- scripts/count.js
+++ scripts/count.js
@@ -8,6 +8,10 @@ const allJobs2 = document.getElementById('all-jobs-cards');
 const interviewJobs = document.getElementById("interview-jobs-cards");  
 const rejectedJobs = document.getElementById('rejected-jobs-cards')  
 
+let aJ = Number(allJobs.children.length);
+let iJ = Number(interviewJobs.children.length);
+let rJ = Number(rejectedJobs.children.length);
+
 function calculateCount(){
     totalCount.innerText = Number(allJobs.children.length);
     totalCount2.innerText = Number(allJobs.children.length);
--- scripts/toggle.js
+++ scripts/toggle.js
@@ -3,11 +3,35 @@ const interviewFilterButton = document.getElementById("interview-filter-button")
 const rejectedFilterButton = document.getElementById("rejected-filter-button");
 
 
-function toggleStyle(id){
+const allJobsDiv = document.getElementById("all-jobs-cards");
+const interviewJobsDiv = document.getElementById("interview-jobs-cards");
+const rejectedJobsDiv = document.getElementById("rejected-jobs-cards");
+
+function toggleStyle(id,parent){
     allFilterButton.classList.remove('btn-primary');
     interviewFilterButton.classList.remove('btn-primary');
     rejectedFilterButton.classList.remove('btn-primary');
 
+    allJobsDiv.classList.add('hidden');
+    interviewJobsDiv.classList.add('hidden');
+    rejectedJobsDiv.classList.add('hidden');
+
     const addTo =document.getElementById(id);
+    const parentHide = document.getElementById(parent);
+
+    const noJob = document.getElementById('no-jobs')
+    noJob.classList.add('hidden')
+
+    const childCount = Number(parentHide.children.length);
+
+
     addTo.classList.add('btn-primary')
+
+    if(childCount===0){
+        noJob.classList.remove("hidden");
+    }
+    else{
+        parentHide.classList.remove("hidden");
+    }
+    // parentHide.classList.remove('hidden')
 }
