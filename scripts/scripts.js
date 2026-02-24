c709bab76c781f40aff460b8cd1d19335a2bf0f1
Mohim129 <countrysidefarm15@gmail.com>
2026-02-24 19:12:37 +0600

    fixed the badge bug, fixed the delete bug, and made the code smaller and more efficient

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@ let interviewList = [];
 let rejectList = [];
 
 const allJobs = document.getElementById("all-jobs-cards");
-const allJobs2 = document.getElementById("all-jobs-cards");
+// const allJobs2 = document.getElementById("all-jobs-cards");
 const interviewJobs = document.getElementById("interview-jobs-cards");
 const rejectedJobs = document.getElementById("rejected-jobs-cards");
 
@@ -15,7 +15,39 @@ const mainContainer = document.querySelector("main");
 const filterDiv = document.getElementById("interview-jobs-cards");
 const filterDiv2 = document.getElementById("rejected-jobs-cards");
 
+function fixJobStatus() {
+  const allCards = document.querySelectorAll("#all-jobs-cards .card");
+  allCards.forEach((card) => {
+    const companyName = card.querySelector(".company-name").innerText;
+    const badge = card.querySelector(".status-badge");
 
+    const isInterview = interviewList.some(
+      (job) => job.companyName.trim() === companyName.trim(),
+    );
+    const isRejected = rejectList.some(
+      (job) => job.companyName.trim() === companyName.trim(),
+    );
+
+    badge.classList.remove(
+      "bg-info-content/10",
+      "text-info-content",
+      "bg-success",
+      "text-white",
+      "bg-error",
+      "text-white",
+    );
+    if (isInterview) {
+      badge.innerText = "Interview";
+      badge.classList.add("bg-success", "text-white");
+    } else if (isRejected) {
+      badge.innerText = "Rejected";
+      badge.classList.add("bg-error", "text-white");
+    } else {
+      badge.innerText = "Not Applied";
+      badge.classList.add("bg-info-content/10", "text-info-content");
+    }
+  });
+}
 
 function calculateCount() {
   totalCount.innerText = Number(allJobs.children.length);
@@ -33,163 +65,103 @@ function calculateCount() {
 calculateCount();
 
 mainContainer.addEventListener("click", function (event) {
+  const jobCard = event.target.closest(".card");
+  if (!jobCard) {
+    return;
+  }
+
   if (event.target.classList.contains("interview-btn")) {
-    const jobCard = event.target.parentNode.parentNode;
+    // const jobCard = event.target.parentNode.parentNode;
     const companyName = jobCard.querySelector(".company-name").innerText;
-    const jobTitle = jobCard.querySelector(".job-position").innerText;
-    const salaryInfo = jobCard.querySelector(".salary-info").innerText;
-    const jobDescription = jobCard.querySelector(".notes").innerText;
-
-    function statusChange() {
-      let badge = jobCard.querySelector(".status-badge");
-      let currentText = badge.innerText;
-
-      if (currentText == "Not Applied" || currentText == "Rejected") {
-
-        badge.classList.remove(
-          "bg-info-content/10",
-          "text-info-content",
-          "bg-error",
-          "text-white",
-        );
-
-        badge.classList.add("bg-success", "text-white");
-        badge.innerText = "Interview";
-        return badge.innerText;
-      } else if (currentText == "Interview") {
-        badge.classList.remove(
-          "bg-success",
-          "text-white",
-          "bg-error",
-          "text-white",
-        );
-        badge.classList.add("bg-info-content/10", "text-info-content");
-        badge.innerText = "Not Applied";
-        return badge.innerText;
-      }
-    }
-
-    const newStatus = statusChange();
-
-    const jobInfo = {
-      companyName,
-      jobTitle,
-      salaryInfo,
-      jobDescription,
-      jobStatus: newStatus,
-    };
-
-
-    const existingInterview = interviewList.find(
-      (item) => item.companyName == jobInfo.companyName,
+    const index = interviewList.findIndex(
+      (job) => job.companyName.trim() === companyName.trim(),
     );
 
-    if (!existingInterview) {
+    if (index > -1) {
+      interviewList.splice(index, 1);
+    } else {
+      const jobInfo = {
+        companyName,
+        jobTitle: jobCard.querySelector(".job-position").innerText,
+        salaryInfo: jobCard.querySelector(".salary-info").innerText,
+        jobDescription: jobCard.querySelector(".notes").innerText,
+        jobStatus: "Interview",
+      };
       interviewList.push(jobInfo);
-      rejectList = rejectList.filter((item) => item.companyName != companyName);
-    } else if (newStatus == "Not Applied") {
-      interviewList = interviewList.filter(
-        (item) => item.companyName != companyName,
+      rejectList = rejectList.filter(
+        (job) => job.companyName.trim() !== companyName.trim(),
       );
     }
-
+    fixJobStatus();
     calculateCount();
     addInterviewJob();
     addRejectedJob();
   } else if (event.target.classList.contains("reject-btn")) {
-    const jobCard = event.target.parentNode.parentNode;
+    // const jobCard = event.target.parentNode.parentNode;
     const companyName = jobCard.querySelector(".company-name").innerText;
-    const jobTitle = jobCard.querySelector(".job-position").innerText;
-    const salaryInfo = jobCard.querySelector(".salary-info").innerText;
-    const jobDescription = jobCard.querySelector(".notes").innerText;
-
-    function statusChange() {
-      let badge = jobCard.querySelector(".status-badge");
-      let currentText = badge.innerText;
-
-      if (currentText == "Not Applied" || currentText == "Interview") {
-
-        badge.classList.remove(
-          "bg-info-content/10",
-          "text-info-content",
-          "bg-success",
-          "text-white",
-        );
-
-        badge.classList.add("bg-error", "text-white");
-
-        badge.innerText = "Rejected";
-
-        return badge.innerText;
-      } else if (currentText == "Rejected") {
-        badge.classList.remove(
-          "bg-error",
-          "text-white",
-          "bg-success",
-          "text-white",
-        );
-        badge.classList.add("bg-info-content/10", "text-info-content");
-        badge.innerText = "Not Applied";
-        return badge.innerText;
-      }
-    }
-    const newStatus = statusChange();
-
-    const jobInfo = {
-      companyName,
-      jobTitle,
-      salaryInfo,
-      jobDescription,
-      jobStatus: newStatus,
-    };
-
-    const existingRejects = rejectList.find(
-      (item) => item.companyName == jobInfo.companyName,
+    const index = rejectList.findIndex(
+      (job) => job.companyName.trim() === companyName.trim(),
     );
 
-    if (!existingRejects) {
+    if (index > -1) {
+      rejectList.splice(index, 1);
+    } else {
+      const jobInfo = {
+        companyName,
+        jobTitle: jobCard.querySelector(".job-position").innerText,
+        salaryInfo: jobCard.querySelector(".salary-info").innerText,
+        jobDescription: jobCard.querySelector(".notes").innerText,
+        jobStatus: "Rejected",
+      };
       rejectList.push(jobInfo);
       interviewList = interviewList.filter(
-        (item) => item.companyName != companyName,
+        (job) => job.companyName.trim() !== companyName.trim(),
       );
-    } else if (newStatus == "Not Applied") {
-      rejectList = rejectList.filter((item) => item.companyName != companyName);
     }
-    interviewList = interviewList.filter(
-      (item) => item.companyName != companyName,
-    );
+    fixJobStatus();
     calculateCount();
-    addRejectedJob();
     addInterviewJob();
+    addRejectedJob();
   }
 });
 
-
 mainContainer.addEventListener("click", function (event) {
   if (event.target.classList.contains("fa-trash-can")) {
-    const card = event.target.closest('.card');
+    const card = event.target.closest(".card");
     if (!card) {
       return;
     }
-    const companyName = card.querySelector('.company-name').innerText;
+    const companyName = card.querySelector(".company-name").innerText.trim();
 
-    const allCards = document.querySelectorAll('#all-jobs-cards .card');
-    allCards.forEach(c => {
-      if (c.querySelector('.company-name').innerText === companyName) c.remove();
+    const allCards = document.querySelectorAll("#all-jobs-cards .card");
+    allCards.forEach((c) => {
+      if (c.querySelector(".company-name").innerText.trim() === companyName.trim())
+        c.remove();
     });
 
-    const interviewCards = document.querySelectorAll('#interview-jobs-cards .card');
-    interviewCards.forEach(c => {
-      if (c.querySelector('.company-name').innerText === companyName) c.remove();
+    const interviewCards = document.querySelectorAll(
+      "#interview-jobs-cards .card",
+    );
+    interviewCards.forEach((c) => {
+      if (c.querySelector(".company-name").innerText.trim() === companyName.trim())
+        c.remove();
     });
 
-    const rejectedCards = document.querySelectorAll('#rejected-jobs-cards .card');
-    rejectedCards.forEach(c => {
-      if (c.querySelector('.company-name').innerText === companyName) c.remove();
+    const rejectedCards = document.querySelectorAll(
+      "#rejected-jobs-cards .card",
+    );
+    rejectedCards.forEach((c) => {
+      if (c.querySelector(".company-name").innerText === companyName)
+        c.remove();
     });
 
-    interviewList = interviewList.filter(job => job.companyName !== companyName);
-    rejectList = rejectList.filter(job => job.companyName !== companyName);
+    interviewList = interviewList.filter(
+      (job) => job.companyName.trim() !== companyName.trim(),
+    );
+    rejectList = rejectList.filter((job) => job.companyName.trim() !== companyName.trim());
+
+    addInterviewJob();
+    addRejectedJob();
     calculateCount();
   }
 });
@@ -201,21 +173,15 @@ function addInterviewJob() {
     let div = document.createElement("div");
     div.className = "card bg-white w-full shadow-sm";
 
-    let badgeClassInfo = "";
-    if (interview.jobStatus === "Interview") {
-      badgeClassInfo = "bg-success text-white";
-    } else if (interview.jobStatus === "Rejected") {
-      badgeClassInfo = "bg-error text-white";
-    } else {
-      badgeClassInfo = "bg-info-content/10 text-info-content";
-    }
-
     div.innerHTML = `
             <div class="card-body">
               <div class="flex justify-between items-center">
                 <h2 class="company-name text-info-content text-xl">
                   ${interview.companyName}
                 </h2>
+                <i
+                  class="fa-regular fa-trash-can text-gray-400 cursor-pointer"
+                ></i>
 
               </div>
               <h4 class="job-position text-[#64748b] mb-4">
@@ -228,9 +194,9 @@ function addInterviewJob() {
               <!-- badge  -->
               <div class="card-actions flex">
                 <button
-                  class="status-badge btn ${badgeClassInfo}"
+                  class="status-badge btn bg-success text-white"
                   disabled
-                >${interview.jobStatus}</button>
+                >Interview</button>
               </div>
 
               <p class="notes text-info-content">
@@ -257,20 +223,15 @@ function addRejectedJob() {
   for (let reject of rejectList) {
     let div = document.createElement("div");
     div.className = "card bg-white w-full shadow-sm";
-    let badgeClassInfo = "";
-    if (reject.jobStatus === "Interview") {
-      badgeClassInfo = "bg-success text-white";
-    } else if (reject.jobStatus === "Rejected") {
-      badgeClassInfo = "bg-error text-white";
-    } else {
-      badgeClassInfo = "bg-info-content/10 text-info-content";
-    }
     div.innerHTML = `
             <div class="card-body">
               <div class="flex justify-between items-center">
                 <h2 class="company-name text-info-content text-xl">
                   ${reject.companyName}
                 </h2>
+                <i
+                class="fa-regular fa-trash-can text-gray-400 cursor-pointer"
+                ></i> 
               </div>
               <h4 class="job-position text-[#64748b] mb-4">
                 ${reject.jobTitle}
@@ -282,9 +243,9 @@ function addRejectedJob() {
               <!-- badge  -->
               <div class="card-actions flex">
                 <button
-                  class="status-badge btn ${badgeClassInfo}"
+                  class="status-badge btn bg-error text-white"
                   disabled
-                >${reject.jobStatus}
+                >Rejected
                 </button>
               </div>
 
